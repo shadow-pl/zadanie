f693d12cab1fb2d27eca31b0e1819a1cbb161f58
Shadow-pl <shadow89@vp.pl>
2010-03-17 17:56:18 +0100

    Poprawiono wszystko poza walidacja update i punktem 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
-
+   def index
+    redirect_to :action => 'show_all'
+  end
   def show_all
     @users = User.all
   end
   def add
     @user = User.new(params[:user])
-    @user.save
+    if request.post?
+      @user.save
+        if @user.save
+          redirect_to :action => "show_all"
+        end
+    end 
   end  
   def show
     @user = User.find(params[:id])
@@ -19,5 +26,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update_attributes(params[:user])
   end
- 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -40,5 +40,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  map.root :controller => "user"
+  map.root :controller => "users"
 end
